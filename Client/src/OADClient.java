4b656d4e77903d18e222c4e7b53fcb672ed96e8c
AdamWW <adamwilhelmi@gmail.com>
2018-03-07 07:47:41 -0500

    Initial Commit

---
+++ Client/src/OADClient.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class OADClient extends JFrame{
+
+    public OADClient() {
+        super("OST ALPHA DRIVER APP");
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
